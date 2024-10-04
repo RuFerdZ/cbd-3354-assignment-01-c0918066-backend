1bdcd0ae8dd90c07b3b1a8bbc96158fd2e4e0def
Rusiru Fernando <37113657+RuFerdZ@users.noreply.github.com>
2024-10-03 20:42:50 -0400

    feat: added endpoint to upload file to google buckets

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,25 @@
-from flask import jsonify, request, render_template, url_for, redirect, flash
+from flask import jsonify, request, url_for, redirect, flash
 from google.cloud import storage
-
 from werkzeug.security import generate_password_hash
-from app import app
+from app import app, db, User
 import os
 
 
-BUCKET_NAME = os.environ.get('BUCKET_NAME')
+BUCKET_NAME = os.environ.get('BUCKET_NAME', 'gke-file-upload')
 
 @app.route('/')
 def index():
     return "Hello, World!"
+
+# Route to upload a file to the bucket
+@app.route('/upload-file', methods=['POST'])
+def upload_blob_from_memory():
+    """Uploads a file to the bucket."""
+    file = request.files.get('file')
+    if file:
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(BUCKET_NAME)
+        blob = bucket.blob(file.filename)
+        blob.upload_from_file(file)
+        return jsonify({'url': blob.public_url})
+    return jsonify({'error': 'A File is required'}), 400
