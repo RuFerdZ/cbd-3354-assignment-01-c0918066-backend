11a4280ed7e86fac86e9897c7334e1a97e528b9e
Rusiru Fernando <37113657+RuFerdZ@users.noreply.github.com>
2024-11-21 19:31:10 -0500

    fix: updated codebase

--- app/__init__.py
+++ app/__init__.py
@@ -3,18 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_cors import CORS
 import secrets
+import dotenv
 
 app = Flask(__name__)
+
+dotenv.load_dotenv()
 
 CORS(app)  # Enable CORS for all routes
 
 secret_key = secrets.token_hex(16)  # 32 characters (16 bytes)
 
 # get from environment
-DB_NAME = os.environ.get('DB_NAME', 'cbd3354')
-USER = os.environ.get('DB_USER', 'cbd3354_usr')
-PASSWORD = os.environ.get('DB_PASSWORD', 'cbd3354_pwd')
-HOST = os.environ.get('DB_HOST', 'localhost')
+DB_NAME = os.environ.get('DB_NAME')
+USER = os.environ.get('DB_USER')
+PASSWORD = os.environ.get('DB_PASSWORD')
+HOST = os.environ.get('DB_HOST')
 PORT = '5432'
 
 # Configure the PostgreSQL database connection
@@ -23,6 +26,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(50), nullable=False)
@@ -30,9 +34,9 @@ class User(db.Model):
     password = db.Column(db.String(), nullable=False)
     phone_no = db.Column(db.String(10), unique=True)
 
+
 # Create the tables if they don't exist
 with app.app_context():
     db.create_all()
 
 from app import routes
-
